3940f38e1b932857d1a4cc996c043bc76d25c903
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-03 16:23:06 -0700

    feat: add header to 'App'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function App() {
-  return <div id="App" className="App"></div>;
+  return (
+    <div id="App" className="App">
+      <h1>MongoDB Test</h1>
+    </div>
+  );
 }
